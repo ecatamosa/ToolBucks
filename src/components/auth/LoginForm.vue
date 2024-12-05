bea0e248fe3a1e1474eb3cc0d9c104fe1470ef31
estarossa258 <gjcabual@gmail.com>
2024-12-05 17:15:13 +0800

    fix: login form redesign

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,20 +1,72 @@
-<template>
-    <v-form fast-fail @submit.prevent>
-                <v-text-field label="Email" variant="outlined"></v-text-field>
+<script setup>
+import { ref } from 'vue'
 
-                <v-text-field label="Password" type="password" variant="outlined"></v-text-field>
+const visible = ref(false)
+</script>
+  <template>
+  <div>
+    <v-img
+      class="mx-auto my-6"
+      max-width="220"
+      src="/public/images/newloginlogo.png"
+    ></v-img>
 
-                <v-checkbox class="py-0" label="Remember password"
-                color="orange-darken-1"></v-checkbox>
+    <v-card
+      class="mx-auto pa-12 pb-8"
+      elevation="8"
+      max-width="448"
+      rounded="lg"
+    >
+      <div class="text-subtitle-1 text-medium-emphasis">Account</div>
 
-                <v-btn
-                  type="submit"
-                  prepend-icon="mdi-login"
-                  block
-                  variant="outlined"
-                  color="orange"
-                  height="48"
-                  >Login</v-btn
-                >
-              </v-form>
+      <v-text-field
+        density="compact"
+        placeholder="Email address"
+        prepend-inner-icon="mdi-email-outline"
+        variant="outlined"
+      ></v-text-field>
+
+      <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+        Password
+
+        <a
+          class="text-caption text-decoration-none text-orange"
+          href="#"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Forgot login password?</a>
+      </div>
+
+      <v-text-field
+        :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+        :type="visible ? 'text' : 'password'"
+        density="compact"
+        placeholder="Enter your password"
+        prepend-inner-icon="mdi-lock-outline"
+        variant="outlined"
+        @click:append-inner="visible = !visible"
+      ></v-text-field>
+
+      <v-checkbox label="Remember password"
+      color="orange"></v-checkbox>
+
+      <v-btn
+        class="mb-8"
+        color="orange"
+        size="large"
+        variant="tonal"
+        block
+      >
+        Log In
+      </v-btn>
+
+      <v-card-text class="text-center">
+        <RouterLink to="/register" class="text-orange text-decoration-none">
+          Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
+        </RouterLink>
+      </v-card-text>
+      
+    </v-card>
+  </div>
 </template>
--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -9,29 +9,8 @@ const { mobile } = useDisplay()
 <template>
   <AppLayout>
     <template #content>
-      <v-container fluid>
-        <v-row>
-        <v-col cols="12" md="6" class="mx-auto">
-          <v-card class="mx-auto" elevation="0" max-width="600">
-            <v-card-title class="text-center">
-              <v-img class="mx-auto" src="/images/tblogo.png" :width="mobile ? '80%' : '50%'"></v-img>
-              <h3 class="font-weight-bold">Login Form</h3>
-            </v-card-title>
-
-            <v-card-text class="bg-surface-light pt-4">
-              <v-divider></v-divider>
-
-              <LoginForm></LoginForm>
-              
-              <v-divider class="my-4"></v-divider>
-              <h5 class="text-center">
-                Don't have an account?
-                <RouterLink class="text-grey text-decoration-none" to="/register">Click here to Register!</RouterLink>
-              </h5>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
+      <v-container fluid class="mx-auto">
+        <LoginForm></LoginForm>
       </v-container>
     </template>
   </AppLayout>
