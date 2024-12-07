60c731c4a0154c0771d4ff4c4f63a16fa6b53236
estarossa258 <gjcabual@gmail.com>
2024-12-07 14:10:45 +0800

    feat: added navbar & carousel in main

--- src/views/system/DashboardView.vue
+++ src/views/system/DashboardView.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { ref } from 'vue';
+import { RouterLink } from 'vue-router';
+import { useTheme } from 'vuetify';
 
 const isDrawerOpen = ref(false);
 
@@ -19,6 +21,23 @@ const user = {
     fullName: 'John Doe',
     email: 'john.doe@doe.com',
   }
+  
+  const colors = [
+    'indigo',
+    'warning',
+    'pink darken-2',
+    'red lighten-1',
+    'deep-purple accent-4',
+  ]
+  const slides = [
+  'images/slide1.png',
+  'images/slide2.png',
+  'images/slide3.png',
+  'images/slide4.png',
+  'images/slide5.png',
+];
+const activeSlide = ref(0);
+
 </script>
 
 <template>
@@ -39,7 +58,43 @@ const user = {
     
     <!-- Center: Categories -->
     <v-row class="justify-center" align="center" dense>
-      <v-btn text color="white">Home</v-btn>
+      <v-btn text color="white">
+        Category   
+    <v-btn variant="plain">
+      <v-icon icon="mdi-chevron-down"></v-icon>
+
+      <v-menu activator="parent">
+        <v-list>
+          <v-list-item v-for="i in 5" :key="i" link>
+            <v-list-item-title>Item {{ i }}</v-list-item-title>
+            <template v-slot:append>
+              <v-icon icon="mdi-menu-right" size="x-small"></v-icon>
+            </template>
+
+            <v-menu :open-on-focus="false" activator="parent" open-on-hover submenu>
+              <v-list>
+                <v-list-item v-for="j in 5" :key="j" link>
+                  <v-list-item-title>Item {{ i }} - {{ j }}</v-list-item-title>
+                  <template v-slot:append>
+                    <v-icon icon="mdi-menu-right" size="x-small"></v-icon>
+                  </template>
+
+                  <v-menu :open-on-focus="false" activator="parent" open-on-hover submenu>
+                    <v-list>
+                      <v-list-item v-for="k in 5" :key="k" link>
+                        <v-list-item-title>Item {{ i }} - {{ j }} - {{ k }}</v-list-item-title>
+                      </v-list-item>
+                    </v-list>
+                  </v-menu>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-btn>
+  
+      </v-btn>
       <v-btn text color="white">Tools</v-btn>
       <v-btn text color="white">Rentals</v-btn>
       <v-btn text color="white">About</v-btn>
@@ -52,7 +107,8 @@ const user = {
     <!-- Right: Login Button -->
     
     <v-spacer></v-spacer>
-    <v-btn  text color="amber-darken-3" class="mr-4" prepend-icon="mdi-account" variant="plain">Login</v-btn>
+    
+      <RouterLink class="text-decoration-none text-orange" to="/login"><v-btn  text color="amber-darken-3" class="mr-4" prepend-icon="mdi-account" variant="plain">Login</v-btn></RouterLink>
   </v-app-bar>
 
     <!-- Hover area on the left -->
@@ -90,17 +146,17 @@ const user = {
           <v-list-item-title>John Doe</v-list-item-title>
         </template> -->
 
+        <!-- Avatar Button -->
         <template v-slot:activator="{ props }">
-  <v-row class="align-center my-2 ms-1">
-    <!-- Avatar Button -->
-    <v-btn icon>
-      <v-avatar
-        color="brown"
-        size="large"
-      >
-        <span class="text-h5">{{ user.initials }}</span>
-      </v-avatar>
-    </v-btn>
+        <v-row class="align-center my-2 ms-1">
+         <v-btn icon v-bind="props">
+           <v-avatar
+           color="brown"
+           size="large"
+            >
+          <span class="text-h5">{{ user.initials }}</span>
+          </v-avatar>
+        </v-btn>
 
     <!-- Name to the right of the avatar -->
     <v-list-item-title class="ml-2 text-white">
@@ -108,8 +164,6 @@ const user = {
     </v-list-item-title>
   </v-row>
 </template>
-
-        
         <v-card>
           <v-card-text>
             <div class="mx-auto text-center">
@@ -118,7 +172,6 @@ const user = {
               >
                 <span class="text-h5">{{ user.initials }}</span>
               </v-avatar>
-              <h3>{{ user.fullName }}</h3>
               <p class="text-caption mt-1">
                 {{ user.email }}
               </p>
@@ -129,17 +182,11 @@ const user = {
               >
                 Edit Account
               </v-btn>
-              <v-divider class="my-3"></v-divider>
-              <v-btn
-                variant="text"
-                rounded
-              >
-                Disconnect
-              </v-btn>
             </div>
           </v-card-text>
         </v-card>
       </v-menu>
+        
       <v-divider class="mt-3"></v-divider>
 
         <v-list-item class="mt-2" prepend-icon="mdi-account" title="My Account" value="account"></v-list-item>
@@ -153,9 +200,27 @@ const user = {
       </template>
     </v-navigation-drawer>
 
+
+
+      <!-- Main Content -->
     <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
-      Main Content
+
+      <v-carousel
+    height="637"
+    :show-arrows="false"
+    cycle
+    hide-delimiter-background
+  >
+    <v-carousel-item v-for="(slide, index) in slides" :key="index">
+        <v-img :src="slide" alt="Slide Image" cover></v-img>
+      </v-carousel-item>
+  </v-carousel>
+
+
     </v-main>
+
+
+
   </v-layout>
 </template>
 
@@ -165,11 +230,11 @@ const user = {
   position: fixed;
   top: 0;
   left: 0;
-  width: 10px; /* Adjust width for sensitivity */
-  height: 100vh;
-  background-color: transparent; /* Make it invisible */
-  z-index: 1000; /* Ensure it's above other content */
-}
+  width: 10px; 
+   height: 100vh; 
+  background-color: transparent; 
+  z-index: 1000; 
+}  
 
 .v-navigation-drawer {
   transition: transform 0.3s ease-in-out;
