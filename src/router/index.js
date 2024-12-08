1defa75c4042307eddfccc5d8c77af1c8892d6bf
estarossa258 <gjcabual@gmail.com>
2024-12-08 19:40:21 +0800

    feat: added routes.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
 import LandingPage from '@/views/system/LandingPage.vue'
 import DashboardView from '@/views/system/DashboardView.vue'
+import { isAuthenticated } from '@/utils/supabase'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,26 +11,32 @@ const router = createRouter({
     {
       path: '/', // Default route
       name: 'landingpage',
-      component: LandingPage
-       // Redirect to the login page
+      component: LandingPage,
+      meta: { requiresAuth: false}
     },
     
     {
       path: '/login',
       name: 'login',
-      component: LoginView
+      component: LoginView,
+      meta: { requiresAuth: false}
     },
     {
       path: '/register',
       name: 'register',
-      component: RegisterView
+      component: RegisterView,
+      meta: { requiresAuth: false}
     },
     {
       path: '/dashboard',
       name: 'dashboard',
-      component: DashboardView
-    }
+      component: DashboardView,
+      meta: { requiresAuth: true}
+    },
+    
   ],
 })
+
+
 
 export default router
